72152af4e6504acf75350ff0d0d755808e789149
Asad3ainJalout <asad3ainjalout@gmail.com>
2017-07-31 19:17:58 -0400

    1)Removed the Quran functionality due to the quran.com api being unable, or me being unable, to call more than the first 10 ayahs of any given surah. 2) Removed the urban dictionary functionality due to it not working properly 3) redid the help message

--- mgbot.py
+++ mgbot.py
@@ -9,6 +9,7 @@
 
 import asyncio
 import discord
+import emoji
 import logging
 import requests
 from lxml import html
@@ -17,38 +18,16 @@ logging.basicConfig(level=logging.ERROR)
 
 client = discord.Client()
 
+# Settings
+prefix = '$'
+
 # Read in the Secret to login
 exec(open('secret.txt').read())
 
-# Code block for the Urban command
-async def urban(request):
-    page = requests.get("https://www.urbandictionary.com/define.php", params={'term':request})
-    tree = html.fromstring(page.content)
-    data = tree.xpath('//*[@id="content"]/div[1]/div[3]/child::node()')
-    print(data.tostring(et, encoding='utf8', method='xml'))
-    return data
-    
-# Code block for the Quran command
-async def quran(command,request):
-    data = []
-    if command == 'info':
-        obj = requests.get("http://staging.quran.com:3000/api/v3/chapters/%s" % (request)).json()['chapter']
-        data.append(obj['name_arabic'])
-        data.append(obj['name_simple'])
-        data.append(requests.get("http://staging.quran.com:3000/api/v3/chapters/%s/info" % (request)).json()['chapter_info']['short_text'])
-        data.append(obj['revelation_order'])
-        data.append(obj['revelation_place'])
-        data.append(obj['verses_count'])
-        data.append(obj['translated_name']['name'])
-        return data
-    elif command == 'verse':
-        request = request.split(":")
-        sura = int(request[0])
-        verse = int(request[1])
-        obj = requests.get("http://staging.quran.com:3000/api/v3/chapters/%s/verses?recitation=1&translations=21&language=en&text_type=words" % (sura)).json()['verses'][verse-1]
-        data.append(obj['text_madani'])
-        data.append(obj['translations'][0]['text'])
-        return data
+# Help Code dictionary
+help = {'mgbot' : '**Main**\n   **' + prefix + 'help** prints out this help. Type ' + prefix + 'help <command> to get more information about a specific command.\n   **' + prefix + 'cone** cones or uncones the shamed (only used by shura)',
+        'help' : '**' + prefix + 'help**\nType ' + prefix + 'help <command> for information about a specific command.\nFor example:\n ' + prefix + 'help cone',
+        }
 
 @client.event
 async def on_ready():
@@ -59,29 +38,11 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.content.startswith('C-C-C-C-C-C-CONVO KILLER #WindowsMasterRace'):
-        await client.send_message(message.channel,' s/Windows/Linux/')
-    if message.content.startswith("You can't tell MuslimGamer-bot what to do, this is America man"):
-        await client.send_message(message.channel,'Exactly, I do what I want.')
-        await client.send_message(message.channel,'http://shirtminion.com/wp-content/uploads/2015/06/Im-Sorry-I-Thought-This-Was-America-South-Park-1.jpg')
-    if message.author.id == ('218150923829116929'):
-        await client.add_reaction(message,'💩')
-    if message.author.id == ('192360529426120704'):
-        await client.add_reaction(message,'💩')
-    if message.content.startswith('$urban'):
-        content = message.content.split(" ")
-        data = await urban(content[1])
-        await client.send_message(message.channel,"```%s```" % (data))
-    elif message.content.startswith('$quran'):
-        content = message.content.split(" ")
-        data = await quran(content[1],content[2])
-        if content[1] == 'info':
-            await client.send_message(message.channel,"```%s\t\t\t%s\t\t\tVerses: %s\n%s\t\t\t%s\t\t\tRevelation Order: %s\n\n%s```" % (data[1],data[0],data[5],data[4],data[6],data[3],data[2]))
-        elif content[1] == 'verse':
-            await client.send_message(message.channel,"```%s\n%s```" % (data[0],data[1]))
-        else:
-            await client.send_message(message.author,"\n_**Main Commands**_\n\t$quran\n_**$quran Commands**_\n\tinfo (surah number)")
-    elif message.content.startswith('$help'):
-        await client.send_message(message.author,"```Main Commands\n--------------------\n\t$help\n\t$quran\n\t\tinfo 1\n\t\tverse 1:1```")
-    
+    if message.content.startswith(prefix):
+        splitMessage = message.content.split(" ")
+        command = splitMessage[0]
+        if command[1:] == 'help':
+            splitMessage.append('mgbot')
+            await client.send_message(message.author, help.get(splitMessage[1], help['mgbot']))
+
 client.run(token)
