9fc74815d60fed06335b8bd8a20e307ffc501aab
Asad3ainJalout <asad3ainjalout@gmail.com>
2017-08-04 20:33:28 -0400

    Remove the Mew heart emoji response

--- mgbot.py
+++ mgbot.py
@@ -32,16 +32,12 @@ cur = con.cursor()
 cur.execute("CREATE TABLE IF NOT EXISTS Cone(id INT PRIMARY KEY, userID INT UNIQUE, coneStatus INT, coneStart INT, coneExpire INT, coneReason TEXT, conedTimes INT)")
 
 # Cone code block
-async def cone_arg_parse(string):
+async def cone(string):
     parser = argparse.ArgumentParser()
     parser.add_argument('command')
     parser.add_argument('-r', nargs='+')
     parser.add_argument('-t')
-
-
-
-async def cone(author,topRole,command,target,reason,time):
-    print('testing')
+    parser.parse_args(string)
 
 # Help Code dictionary
 help = {
@@ -67,9 +63,6 @@ async def on_message(message):
             await client.send_message(message.author, help.get(splitMessage[1], help['mgbot']))
         #Cone function, we determine what variables are given before calling the cone function
         elif command[1:] == 'cone':
-            await cone(message.author.id, message.author.top_role.id, something, message.mentions, reason, time)
-    # Heart emoji reaction to mew at the request of bloodmoney
-    elif message.author.id == '229345661454123008':
-        await client.add_reaction(message,'♥')
+            await cone(splitMessage)
 
 client.run(token)
