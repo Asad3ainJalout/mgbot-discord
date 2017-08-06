82c0f9fe0a34b5bf87cc94b2fad67e716a65cb55
Asad3ainJalout <asad3ainjalout@gmail.com>
2017-08-06 00:31:44 -0400

    Fleshing out the cone functions, Code currently broken

--- mgbot.py
+++ mgbot.py
@@ -11,6 +11,7 @@ import asyncio
 import argparse
 import discord
 import logging
+from pytimeparse import parse
 import requests
 import sqlite3 as lite
 from lxml import html
@@ -29,15 +30,28 @@ exec(open('secret.txt').read())
 con = None
 con = lite.connect('mgbot.db')
 cur = con.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS Cone(id INT PRIMARY KEY, userID INT UNIQUE, coneStatus INT, coneStart INT, coneExpire INT, coneReason TEXT, conedTimes INT)")
+cur.execute("CREATE TABLE IF NOT EXISTS Cone(userID INT PRIMARYKEY, coneStatus INT, coneStart INT, coneExpire INT, coneReason TEXT, conedTimes INT, coner TEXT)")
 
 # Cone code block
-async def cone(string):
+async def cone_argsparse(string):
     parser = argparse.ArgumentParser()
     parser.add_argument('command')
+    parser.add_argument('victims', nargs='+')
     parser.add_argument('-r', nargs='+')
     parser.add_argument('-t')
-    parser.parse_args(string)
+    return parser.parse_args(string)
+
+async def cone(userID,command,coneStart,coneDuration,coneReason,coner, channel):
+    if command == 'set' and (message.author.top_role.id == '287369489987928075' or message.author.top_role.id == '193105896010809344' or message.author.top_role.id == '192322577207787523'):
+        pass
+    if command == 'unset' and (message.author.top_role.id == '287369489987928075' or message.author.top_role.id == '193105896010809344' or message.author.top_role.id == '192322577207787523'):
+        pass
+    if command == 'info':
+        if cur.fetchone():
+            await client.send_message(channel, cur.execute("SELECT coneStart, coneExpire, coneReason, conedTimes, coner FROM Cone WHERE userID = '%s'" % userID))
+        else:
+            await client.send_message(channel,'This user has never been coned')
+    return
 
 # Help Code dictionary
 help = {
@@ -61,8 +75,21 @@ async def on_message(message):
         if command[1:] == 'help':
             splitMessage.append('mgbot')
             await client.send_message(message.author, help.get(splitMessage[1], help['mgbot']))
-        #Cone function, we determine what variables are given before calling the cone function
+        #Cone function, we determine what variables are given before calling the cone_argsparse function
         elif command[1:] == 'cone':
-            await cone(splitMessage)
+            args = await cone_argsparse(splitMessage[1:])
+            
+            #checking if the variables defined or not
+            try:
+                args.t
+            except:
+                args.t = '2000y'
+            try:
+                args.r
+            except:
+                args.r = 'You have been coned for reasons unknown.'
+            
+            for victim in args.victims:
+                await cone(victim[1:],args.command,message.timestamp,parse(args.t),args.r,message.author.name.id,message.channel)
 
 client.run(token)
